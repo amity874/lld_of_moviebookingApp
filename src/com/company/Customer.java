ca85c5c54d9ca287ebc3da7142b552c06ad220e4
Amit <amity552000@gmail.com>
2021-09-24 21:34:59 +0530

    Customer and user class

---
+++ src/com/company/Customer.java
@@ -0,0 +1,8 @@
+package com.company;
+import java.util.List;
+
+public class Customer extends User{
+        public Customer(int id, String name, Address address, int age, List<Long> phoneNumbers) {
+            super(id, name, address, age, phoneNumbers);
+        }
+}
---
+++ src/com/company/Language.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public enum Language {
+    HINDI,ENGLISH,FRENCH
+}
